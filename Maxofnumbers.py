7c780cdbbbaa9896648dcc9145f6d89b5f032dde
cjspiteri <tapherjspiteri@users.noreply.github.com>
2021-03-25 19:32:49 -0400

    Create Maxofnumbers.py

---
+++ Maxofnumbers.py
@@ -0,0 +1,7 @@
+# 1. Write a Python function to find the Max of three numbers
+# https://www.w3resource.com/python-exercises/python-functions-exercises.php
+
+
+numbers = [853,634,644]
+answer = max(numbers)
+print (answer)

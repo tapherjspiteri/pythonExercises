d52f678b87d22daaeda5eaeca8acffb91927308b
cjspiteri <tapherjspiteri@users.noreply.github.com>
2021-03-25 19:37:49 -0400

    Create sumlist.py

---
+++ sumlist.py
@@ -0,0 +1,9 @@
+# 2. Write a Python function to sum all the numbers in a list.
+
+# https://www.w3resource.com/python-exercises/python-functions-exercises.php
+
+
+numbers = [3,4,10]
+
+answer = sum(numbers)
+print (answer)
